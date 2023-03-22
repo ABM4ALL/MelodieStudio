b0c00504c525af7a1f4a1cce1fc3aa6180644ef9
hzy15610046011 <1295752786@qq.com>
2023-03-22 10:35:10 +0800

    modified props

--- src/components/flowchart/VueNode.vue
+++ src/components/flowchart/VueNode.vue
@@ -1,59 +1,70 @@
 <template>
-    <div class="demo-collapse" :style="properties.style">
-        <div>当前值：{{ text }}</div>
-        <div class="demo-operator">
-            <el-input @keydown.stop type="text" v-model="increaseFactor" />
-            <el-button @mousedown.stop @click="$emit('btnClick', increaseFactor)">增加</el-button>
-        </div>
+  <div class="demo-collapse" :style="properties.style">
+    <div>当前值：{{ text }}</div>
+    <div class="demo-operator">
+      <el-input v-model="increaseFactor" />
+      <el-button @mousedown.stop @click="$emit('btnClick', increaseFactor)"
+        >增加</el-button
+      >
+      <el-input @keydown.stop v-model="t123"  />
     </div>
+  </div>
 </template>
   
 <script lang="ts">
-import { PropType, defineComponent } from 'vue';
+import { PropType, defineComponent } from "vue";
 
 export default defineComponent({
-    props: {
-        properties: {
-            type: Object as PropType<{
-                t: number,
-                style: any
-            }>,
-            default: () => ({
-                t: 1,
-                style: {},
-            }),
-        },
-        text: String,
+  props: {
+    properties: {
+      type: Object as PropType<{
+        t: number;
+        style: any;
+      }>,
+      default: () => ({
+        t: 1,
+        style: {},
+      }),
     },
-    data() {
-        return {
-            showTitle: "",
-            increaseFactor: 0,
-        };
+    text: String,
+  },
+  data() {
+    return {
+      t123: "",
+      showTitle: "",
+      increaseFactor: 0,
+    };
+  },
+  emits: ["btnClick", "valueChange"],
+  mounted() {
+    console.log(this);
+  },
+  methods: {
+  },
+  watch: {
+    "properties.t": {
+      handler(val) {
+        this.increaseFactor = val;
+      },
+      immediate: true,
     },
-    emits: ["btnClick"],
-    mounted() {
-        console.log(this);
-    },
-    watch: {
-        "properties.t": {
-            handler(val) {
-                this.increaseFactor = val;
-            },
-            immediate: true,
-        },
-    },
+    t123:{
+        handler(val){
+            this.$emit("valueChange", val)
+        }
+    }
+  },
 });
 </script>
 <style scoped>
 .demo-collapse {
-    width: 300px;
-    height: 100px;
-    border: 1px solid #444;
-    box-sizing: border-box;
+  width: 300px;
+  height: 100px;
+  border: 1px solid #444;
+  box-sizing: border-box;
 }
 
 .demo-operator {
-    display: flex;
+  display: flex;
 }
 </style>
