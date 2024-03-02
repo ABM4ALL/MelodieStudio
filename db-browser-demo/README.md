c6875032f4ab771998af9958d5cdea683865796a
hzy14610046011 <1295752786@qq.com>
2024-03-02 11:53:23 +0800

    removed unused files

--- db-browser-demo/README.md
+++
@@ -1,4 +0,0 @@
-## Packup in docker
-Under this directory:
-python packup.py
-docker build -t db:v0.1 .
--- db-browser-demo/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .orm import Base
-from .database import *
-
-# 创建表结构
-Base.metadata.create_all(engine)
